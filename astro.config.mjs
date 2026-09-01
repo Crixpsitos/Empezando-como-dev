624975eb5bb40c27c614aa5df2afa0d1edd61d56
Crixpsitos <cristianpe2609@gmail.com>
2026-09-01 15:05:22 -0500

    add hero section and terminal

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import appHosting from '@apphosting/astro-adapter';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    adapter: appHosting({
-        mode: 'standalone'
-    })
+  output: 'server',
+
+  adapter: appHosting({
+      mode: 'standalone'
+  }),
+
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [react()]
 });
